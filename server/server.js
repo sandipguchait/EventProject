6e5f3c6aaeb7fc6859d08d1b636ffc9f65614023
Sandip Guchait <35764971+sandipguchait@users.noreply.github.com>
2019-05-25 00:51:37 +0530

    fixing bugs

--- server/server.js
+++ server/server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const nodemailer = require('nodemailer');
 
 const Data = require('./data');
 
